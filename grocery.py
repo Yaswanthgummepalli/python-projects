904df78c9504ae5551d897d84b74ae2bddfe6f73
yaswanth <yashu2005md@gmail.com>
2025-07-14 18:56:47 +0530

    grocery is added first time

---
+++ grocery.py
@@ -0,0 +1,48 @@
+class Grocery:
+    def __init__(self):
+        self.list=[]
+
+    def add(self,data):
+        if data in self.list:
+            print("already exist")
+        else:
+            self.list.append(data)
+            print(self.list)
+
+    def remove(self,data):
+        if data in self.list:
+            self.list.remove(data)
+            print(self.list)
+        else:
+            print(f"{data}not exist")
+
+    def view(self):
+        print("milk")
+        print("water")
+        print("curd")
+        print("oil")
+        print("lemon")
+
+    def exit(self):
+        print("your list:",*self.list,sep=",")
+if __name__=="__main__":
+    g = Grocery()
+    
+
+    while True:
+        print("choose your option")
+        option = int(input("enter the number:"))
+        if option == 1:
+            g.view()
+        elif option == 2:
+            item = input("enter the item:")
+
+            g.add(item)
+        elif option == 3:
+            item1 = input("enter the item:")
+            g.remove(item1)
+        else:
+            g.exit()
+            if option==4:
+                break
+    print("welcome again")
